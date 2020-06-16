80d52fc7d25b56679cdb22bfc31113acee575353
nayara.pimentel <n.pimentel@vallourec.com>
2020-06-16 19:40:28 -0300

    Questao 1 ao 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,14 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'posts',
+    loadChildren: () => import('./posts/posts.module').then( m => m.PostsPageModule)
+  },
+  {
+    path: 'adicionar-posts',
+    loadChildren: () => import('./adicionar-posts/adicionar-posts.module').then( m => m.AdicionarPostsPageModule)
   }
 ];
 
